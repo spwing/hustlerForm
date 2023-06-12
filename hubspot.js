4f81921c7e3cb1b7d3354b8ab922f59e44fd70e5
spwing <spwing@mit.edu>
2023-06-12 11:35:29 -0700

    Update hubspot.js

--- hubspot.js
+++ hubspot.js
@@ -10,7 +10,7 @@ window.onload = function() {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.API_KEY}`
+        'Authorization': `Bearer pat-na1-4a1d75f9-05b9-4ab2-a7ed-4fc7260b22f6`
       },
       body: JSON.stringify({
         properties: [
